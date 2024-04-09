82bf75dc3326453c726545d292abe57f72e2a213
Maximiliano Cerra <cerramaximiliano@gmail.com>
2024-04-09 20:04:50 -0300

    feature/ production bullder

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.listen('3030', () => {
+    console.log('Listen On Port 3030')
+});
+
+const path = require('path');
+app.use(express.static(path.resolve(__dirname, '../dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../dist/index.html'))
+});
